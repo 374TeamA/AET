4ca6baaf5becd3f3cb9b9b4b3a6109bb90a78252
Maverick-Run <jake.postlewaight@gmail.com>
2023-09-24 00:37:45 +1200

    Added some todos for future additions

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -5,12 +5,13 @@ import Accounts from "./pages/Accounts";
 import Reports from "./pages/Reports";
 import Settings from "./pages/Settings";
 import Dashboard from "./pages/Dashboard";
+// TODO: Setup routes to allow for dynamic accounts routing based on accounts added by the user
 const Routers: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/accounts" element={<Accounts />} />
+        <Route path="/" element={<Dashboard />} /> {/* Creates a route for the root path to lead to dashboards (IE index is dashboard component) */}
+        <Route path="/accounts" element={<Accounts />} /> {/* Creates a route for accounts */}
         <Route path="/reports" element={<Reports />} />
         <Route path="/settings" element={<Settings />} />
       </Routes>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+//TODO: Set it up so if there is no accounts the dashboard prompts to create a new account 
 export default function Dashboard() {
   return (
     <div>Dashboard</div>
