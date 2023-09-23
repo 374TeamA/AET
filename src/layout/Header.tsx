11e48dfc3788b9edf75f0445ca70a4eacba5c021
Maverick-Run <jake.postlewaight@gmail.com>
2023-09-24 00:33:36 +1200

    - Logo in Header - Sidebar styling setup more robust

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div className='header'>Automated Expenses Tracker</div>
+    <div className='header'><img src="/holistic_finance_original_logo.png" />
+    <p style={{paddingLeft:'2rem', fontWeight:'500', fontSize:'1.5rem'}}>Automated Expenses Tracker</p>
+    </div>
   )
 }
--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "../styles/nav.css";
 export default function Sidebar() {
   return (
     <div className="sidebar">
-      <ul>
+      <ul className="navbar">
         <li>
           <a href="/">Dashboard</a>
         </li>
         <li>
           <a href="/accounts">Accounts</a>
         </li>
-        <li>
-          <a href="/settings">Settings</a>
-        </li>
-        {/* Add more links for other routes */}
       </ul>
+      <div className="bottom-links">
+        <ul>
+          <li>
+            <a href="/reports">Reports</a>
+          </li>
+          <li>
+            <a href="/settings">Settings</a>
+          </li>
+          {/* Add more links for other routes */}
+        </ul>
+      </div>
     </div>
   );
 }
