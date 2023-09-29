063aed57f6d0d9279b386cd15d474131d56b48b1
Xehny <nathan@pook.nz>
2023-09-29 17:21:58 +1300

    Get transaction functionality working

--- src/database/initialisation.ts
+++ src/database/initialisation.ts
@@ -1,9 +1,9 @@
 import { Transaction } from "../types/transaction";
 
-export function connectToDatabase(): Promise<IDBDatabase> {
+export function connectToDatabase(acc: string): Promise<IDBDatabase> {
   return new Promise((resolve, reject) => {
-    if (!('indexedDB' in window)) {
-      reject('Your browser does not support IndexedDB');
+    if (!("indexedDB" in window)) {
+      reject("Your browser does not support IndexedDB");
     }
 
     const req = indexedDB.open("AET", 1);
@@ -11,29 +11,30 @@ export function connectToDatabase(): Promise<IDBDatabase> {
     req.onerror = () => {
       console.error(`IndexedDB Error: ${req.error}`);
       reject("Error opening database");
-    }
+    };
 
     req.onupgradeneeded = () => {
-      console.log("DB Created/updated")
+      console.log("DB Created/updated");
       const db = req.result;
 
-      if (!db.objectStoreNames.contains("transactions")) {
-        const tOS = db.createObjectStore("transactions", {keyPath: "id"});
-        tOS.createIndex("date", "date", {unique: false});
-        tOS.createIndex("merchant", "merchant", {unique: false});
+      if (!db.objectStoreNames.contains(acc)) {
+        const tOS = db.createObjectStore(acc, { keyPath: "id" });
+        tOS.createIndex("date", "date", { unique: false });
+        tOS.createIndex("merchant", "merchant", { unique: false });
       }
-    }
+    };
 
     req.onsuccess = () => {
       console.log("Successful database connection");
       const db = req.result;
-      resolve(req.result);
 
-      db.onversionchange = () => {
-        db.close();
-        alert("Database is outdated, please reload the page.")
-      };
-      console.log("progress1.5")
+      if (!db.objectStoreNames.contains(acc)) {
+        const tOS = db.createObjectStore(acc, { keyPath: "id" });
+        tOS.createIndex("date", "date", { unique: false });
+        tOS.createIndex("merchant", "merchant", { unique: false });
+      }
+
+      resolve(db);
     };
   });
 }
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,13 @@
 // import React from 'react'
-import { saveTransaction } from "../database/transactions";
-import { deleteTransaction } from "../database/transactions";
+import {
+  saveTransaction,
+  deleteTransaction,
+  getTransactions
+} from "../database/transactions";
+
 import { Transaction } from "../types/transaction";
 
-//TODO: Set it up so if there is no accounts the dashboard prompts to create a new account 
+//TODO: Set it up so if there is no accounts the dashboard prompts to create a new account
 export default function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+  return <div>Dashboard</div>;
 }
