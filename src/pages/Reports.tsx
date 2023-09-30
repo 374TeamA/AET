c96685c22d4bdab54b05ac6c4549f0c13312199e
Maverick-Run <jake.postlewaight@gmail.com>
2023-09-30 15:22:49 +1300

    buttons do things

--- src/pages/Reports.tsx
+++ src/pages/Reports.tsx
@@ -1,11 +1,15 @@
 // import React from 'react'
 // TODO: Makayla will create some react components to generate charts from an array of transactions
 import Chart from "chart.js/auto";
+import { useState, ChangeEvent } from "react";
 import { generateGraph } from "../functions/generateGraph";
 import "../styles/reports.css";
 import Group from "../components/Group";
 
 export default function Reports() {
+  const [startDate, setStartDate] = useState<string>("");
+  const [endDate, setEndDate] = useState<string>("");
+
   const createGraph = () => {
     // Get a reference to the select element
     const selectElement: HTMLSelectElement = document.getElementById(
@@ -29,28 +33,72 @@ export default function Reports() {
     new Chart(canvas, recieved);
   };
 
+  const handleExportTransactions = () => {
+    throw new Error("Function not implemented.");
+  };
+
+  const handleExportGraph = () => {
+    throw new Error("Function not implemented.");
+  };
+
+  const handleStartDateChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newDate: string = event.target.value;
+    setStartDate(newDate);
+  };
+
+  const handleEndDateChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newDate: string = event.target.value;
+    setEndDate(newDate);
+  };
+
+  const handleDays = (days: number) => {
+    const today: Date = new Date();
+    const startDate: Date = new Date();
+    startDate.setDate(today.getDate() - days);
+    setStartDate(startDate.toISOString().split("T")[0]);
+    setEndDate(today.toISOString().split("T")[0]);
+  };
+
   return (
     <div className="padding-2">
       <p>Reports</p>
       <Group label="Export">
         <div>
           <label htmlFor="startDate">Start Date:</label>
-          <input type="date" id="startDate" />
+          <input type="date" id="startDate" onChange={handleStartDateChange} />
         </div>
         <div>
           <label htmlFor="endDate">End Date:</label>
-          <input type="date" id="endDate" />
+          <input type="date" id="endDate" onChange={handleEndDateChange} />
         </div>
-        <button>7 Days</button>
-        <button>14 Days</button>
-        <button>1 month</button>
+        <button
+          onClick={() => {
+            handleDays(7);
+          }}
+        >
+          7 Days
+        </button>
+        <button
+          onClick={() => {
+            handleDays(14);
+          }}
+        >
+          14 Days
+        </button>
+        <button
+          onClick={() => {
+            handleDays(31);
+          }}
+        >
+          1 month
+        </button>
         <label htmlFor="allTransactions" className="custom-checkbox">
           All Transactions
           <input type="checkbox" id="allTransactions" />
           <span className="checkmark"></span>
         </label>
-        <button>Export CSV</button>
-        <button>Export Graph</button>
+        <button onClick={handleExportTransactions}>Export CSV</button>
+        <button onClick={handleExportGraph}>Export Graph</button>
       </Group>
 
       <select id="typeSelection">
