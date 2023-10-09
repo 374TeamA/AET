e3cd2bf7d182734052dc60aecbfb0b587dc804a3
Maverick-Run <jake.postlewaight@gmail.com>
2023-10-10 11:07:55 +1300

    Update to context providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,29 @@
-import { useState } from "react";
-import { AccountContext, AccountUpdaterContext, defaultAccounts } from "./context/AccountsContext.ts";
-import { CategoryContext, CategoryList, CategoryUpdaterContext, defaultCategories } from "./context/CategoryContext.ts";
+// import { useState } from "react";
+import { AccountsProvider } from "./context/AccountsContext.tsx";
+import { CategoryProvider } from "./context/CategoryContext.tsx";
 import Root from "./layout/Root.tsx";
 //import { BrowserRouter as Router } from "react-router-dom";
 import Routers from "./Routes.tsx";
-import { Account } from "./types/account.ts";
+//import { Account } from "./types/account.ts";
 function App() {
   /* 
   Setup AccountContext (Storing the list of users' accounts)
   */
   // state to store the list of categories
-  const [accountList, setAccountList] =
-    useState<Account[]>(defaultAccounts);
-  
+  //const [accountList, setAccountList] = useState<Account[]>(defaultAccounts);
+
   /* 
   Setup CategoryContext (Storing the list of current categories)
   */
-  const [categoryList, setCategoryList] =
-  useState<CategoryList>(defaultCategories);
 
   return (
-    <AccountContext.Provider value={accountList}>
-      <AccountUpdaterContext.Provider value={setAccountList}>
-        <CategoryContext.Provider value={categoryList}>
-          <CategoryUpdaterContext.Provider value={setCategoryList}>
-            <Root>
-              <Routers />
-            </Root>
-          </CategoryUpdaterContext.Provider>
-        </CategoryContext.Provider>
-      </AccountUpdaterContext.Provider>
-    </AccountContext.Provider>
+    <AccountsProvider>
+      <CategoryProvider>
+        <Root>
+          <Routers />
+        </Root>
+      </CategoryProvider>
+    </AccountsProvider>
   );
 }
 
---
+++ src/context/AccountsContext.tsx
@@ -0,0 +1,44 @@
+import {
+  Dispatch,
+  SetStateAction,
+  createContext,
+  useEffect,
+  useState
+} from "react";
+import { Account } from "../types/account";
+import { getAccounts } from "../database/accounts";
+
+// Load in the default accounts
+// create a map with all the default categories (and their displayName the same as their ID)
+//export const defaultAccounts: Account[] = await getAccounts();
+/*[
+  "Westpac",
+].map(x=>({id:x,name:x}))*/
+
+// The defaultValue argument is only used when a component does not have a matching Provider above it in the tree. This default value can be helpful for testing components in isolation without wrapping them
+// (React Docs https://legacy.reactjs.org/docs/context.html#reactcreatecontext)
+export const AccountContext = createContext<Account[]>([]);
+
+export const AccountUpdaterContext = createContext<
+  Dispatch<SetStateAction<Account[]>>
+>((accounts) => accounts);
+
+export function AccountsProvider({ children }: { children: React.ReactNode }) {
+  const [accounts, setAccounts] = useState<Account[]>([]);
+
+  useEffect(() => {
+    getAccounts().then(setAccounts);
+  }, []);
+
+  useEffect(() => {
+    console.log("Accounts updated");
+  }, [accounts]);
+
+  return (
+    <AccountContext.Provider value={accounts}>
+      <AccountUpdaterContext.Provider value={setAccounts}>
+        {children}
+      </AccountUpdaterContext.Provider>
+    </AccountContext.Provider>
+  );
+}
