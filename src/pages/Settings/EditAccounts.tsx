8c93b0971378bf469921ccee202f6dabe03a59d2
Bert Downs <bertkdowns@gmail.com>
2023-10-16 23:40:17 +1300

    Editing accounts can be cancelled

--- src/pages/Settings/EditAccounts.tsx
+++ src/pages/Settings/EditAccounts.tsx
@@ -51,25 +51,25 @@ export default function EditAccounts() {
   };
   const updateSelectedAccount = () => {
     if (accountList) {
-      // update the account name for the selected account
       const newAccountList = [...accountList];
+      if(selectedAccount == accountList.length) {
+        newAccountList.push({ name: "", id: uuidv4() });
+      }
+      // update the account name for the selected account
       newAccountList[selectedAccount].name = newAccountName;
       setAccountList(newAccountList);
       setEditDialogOpen(false);
-      accountList && saveAccount(accountList[selectedAccount]);
+      accountList && saveAccount(newAccountList[selectedAccount]);
     }
   };
 
   const addAccount = () => {
     // adds a account to the list
     if (accountList) {
-      const newAccountList = [...accountList];
-      newAccountList.push({ name: "", id: uuidv4() });
-      setAccountList(newAccountList);
       // allow the user to enter a name for the account:
       setEditDialogOpen(true);
       setNewAccountName("");
-      setSelectedAccount(newAccountList.length - 1);
+      setSelectedAccount(accountList.length); // this is out of bounds of the current accounts, so indicates adding a new item
     }
   };
 
